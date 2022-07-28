b2b685257f74b2f3dc7d72f52fcaca8dc8849549
Juan Carlos Ruiz <klaufel@gmail.com>
2022-07-28 17:28:30 +0200

    remove destructuring

--- config/src/index.ts
+++ config/src/index.ts
@@ -1,9 +1,7 @@
-const { NODE_ENV, VERCEL_URL } = process.env;
-
 const apiUrl =
-  NODE_ENV === 'development'
+  process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : `https://${VERCEL_URL}`;
+    : `https://${process.env.VERCEL_URL}`;
 
 const config = {
   apiUrl,
